add99cb5b099742b4f35fb00fd4929253dfd83bd
LudoTheHUN <ludoart@yahoo.com>
2014-05-16 23:46:31 +0100

    starting on some benchmarking and epsilon to rho setting for resonable pp

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.mikera/core.matrix "0.22.0"]
-                 [net.mikera/vectorz-clj "0.22.0"]])
+                 [net.mikera/vectorz-clj "0.22.0"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.3"]]}})
 
 
--- src/pperceptrons/core.clj
+++ src/pperceptrons/core.clj
@@ -102,7 +102,7 @@
 (pp-output [[-4.0 0.3 -0.3 0.5 -0.1]
     [0.1 -0.2 -0.4 -0.6 0.5]
     [0.2 -0.1 -0.7 -0.6 0.2]
-    [0.2 -1.1 -0.7 -0.6 0.2]] [-0.2 0.2 0.3 1.0] 3)
+    [0.2 -1.1 -0.7 -0.6 0.2]] [-0.4 0.2 0.3 1.0] 2)
 
 (pp-output pperceptron [-0.2 0.2 0.3 1.0] 3)
 (pp-output pperceptron [-10.2 0.2 0.6 1.0] 10)
@@ -138,8 +138,10 @@
 
 
 (defn scaling-to-one-fn [perceptron eta--learning-rate]
-   (m-ops/* perceptron (* -1.0 eta--learning-rate (- (m/length-squared perceptron) 1.0)))
-  )
+   (m-ops/* perceptron (* -1.0 eta--learning-rate (- (m/length-squared perceptron) 1.0))))
+
+
+
 
 (scaling-to-one-fn [0.2 0.0 0.4] 0.01)
 
@@ -324,7 +326,7 @@ input
            (:gamma--margin-around-zero pp)
           )))
   (train-seq [pp input-output-seq] :WIP)
-  (anneal-eta [pp] (assoc-in pp [:eta--learning-rate] 99 ))
+  (anneal-eta [pp] (update-in pp [:eta--learning-rate] (fn [x] (* x 0.999)) ))  ;;WIP there is a specific algo for this...
   )
 
 
@@ -344,23 +346,25 @@ input
 
 ;;TODO DONE  constructing close to length one perceptrons from the start...
 ;;TODO take the random output and assuming it's a pperceptron, scale each perceptron towards length 1.0
+
 (defn scale-to-size-one
  ([pperceptron] (scale-to-size-one (imp/get-implementation-key pperceptron) pperceptron ))
  ([matrix-implementation pperceptron]
-   (m/matrix matrix-implementation (map (fn [perceptron] (m/div perceptron (m/length perceptron)))   (m/slices pperceptron)) )))
-
-#_(time (map m/length (m/slices (scale-to-size-one (uniform-dist-matrix-center-0 :vectorz [5 100] 42)))))
+   (m/matrix matrix-implementation (map m/normalise (m/slices pperceptron)))))
 
 
 
-(defn make-resonable-pp [inputsize  ;;do a (count input)
-                         epsilon    ;;less then 0.5, nice numers here are eg: 0.25, 0.1
-                         zerod?     ;;if true, n, number of perceptrons, will be even hence zero will be a possible output.
-                         seed       ;;some int of your choosing
-                         ]
+
+(defn make-resonable-pp
+ ([inputsize  epsilon zerod?  seed] (make-resonable-pp inputsize epsilon zerod? seed 1))
+ (                    [inputsize  ;;do a (count input)
+                       epsilon    ;;less then 0.5, nice numers here are eg: 0.25, 0.1
+                       zerod?     ;;if true, n, number of perceptrons, will be even hence zero will be a possible output.
+                       seed       ;;some int of your choosing
+                       size-boost] ;;How many times should the pp be bigger then recomended
  (let [matrix-implementation  :vectorz
        pwidth      (+ inputsize 1)
-       n-prez (int (/ 2 epsilon))
+       n-prez (int (* (/ 2 epsilon) size-boost))
        n     (cond (and zerod? (even? n-prez))
                      n-prez
                    (and zerod? (odd? n-prez))
@@ -370,10 +374,11 @@ input
                    (and (not zerod?) (even? n-prez))
                      (+ 1 n-prez)
                    :else :this_should_never_happen!)
-       rho-wip  (int (/ 1 (* 2 epsilon)))
+       rho-wip  (int (/ 1 (* 1 epsilon)))  ;;paper says 2, but this does not reconcile with example on page 6
        rho      (if (= rho-wip 0) 1 rho-wip)]
   (new pperceptron-record
-   (scale-to-size-one (uniform-dist-matrix-center-0 matrix-implementation [n pwidth] seed))   ;; pperceptron ; the matrix holding the paralel perceptron weights which is as wide as the input +1 and as high as the number of perceptrons, n.
+  ; (scale-to-size-one (uniform-dist-matrix-center-0 matrix-implementation [n pwidth] seed))   ;; pperceptron ; the matrix holding the paralel perceptron weights which is as wide as the input +1 and as high as the number of perceptrons, n.
+   (uniform-dist-matrix-center-0 matrix-implementation [n pwidth] seed)
    matrix-implementation     ;; As supported by core.matrix, tested against  :persistent-vector and :vectorz
    n                         ;; n ; the total number of perceptrons in the pperceptron
    pwidth                    ;; size of each perceptron , width of pperceptron, +1 to size of input
@@ -382,15 +387,30 @@ input
    rho                       ;; rho--squashing-parameter ; An int. If set to 1, will force the pp to have binary output (-1,+1) in n is odd. Can be at most n. Typically set to  (/ 1 (* 2 epsilon))
    1.0                       ;; mu-zeromargin-importance ; The zero margin parameter. Typically 1.
    0.5                       ;; gamma--margin-around-zero ; Margin around zero of the perceptron. Needs to be controlled for best performance, else set between 0.1 to 0.5
-  )))
+  ))))
 
 
 
 
 
-(def pp-a (make-resonable-pp 1 1.0 false 42))
+(def pp-a
+  (make-resonable-pp 1 0.5 false 42)
+  )
 
-(make-resonable-pp 1 1.0 true 42)
+(anneal-eta (make-resonable-pp 1 1.0 true 42))
+
+(/ 2 50.0)
+(read-out (make-resonable-pp 1 0.1 false 42 10) [1.1549])
+
+(let [pp (make-resonable-pp 1 0.01 false 41 10)]
+  (map  (fn [x] (read-out pp [x])) (range -2 2 0.1)))
+
+
+
+(let [pp (make-resonable-pp 2 0.75 false 42 1)]
+  (map  (fn [x] (read-out pp [-0.9 x])) (range -3 3 0.01)))
+
+
 
 (read-out (make-resonable-pp 2 0.27 true 42) [2 1])
 (train (make-resonable-pp 2 0.27 true 42) [2 1] 1)
